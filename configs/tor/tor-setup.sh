5745d3124a3e96faccc7143457807b9c1d45e593
adamiaonr <adamiaonr@gmail.com>
2015-04-24 16:39:12 -0400

    Added script to 'wake up' Tor testbed
    Added script which 'wakes up' Tor testbed from the node-gw node alone
    (no need to ssh into each machine and start Tor individually).
    Also starts tcpdump captures and Internet access if specified as an
    option.

---
+++ configs/tor/tor-setup.sh
@@ -0,0 +1,96 @@
+# script to run (and measure stuff from) tor ORs, from node-gw in one stroke
+# created by: antonior@andrew.cmu.edu
+
+#!/bin/bash
+
+PEM_FILE=/home/$USER/.ssh/node1.pem
+
+TOR_DATA_DIR=/home/$USER/workbench/accelerator-misc/
+# default torrc file (should be used most of the times)
+TOR_TORRC=torrc
+
+TOR_DIR=/home/$USER/workbench/accelerator/tor
+TOR_OR_DIR=/src/or
+TOR_BINARY=tor
+
+NODE_IPS=("172.31.100.10" "172.31.100.20" "172.31.100.30" "172.31.100.40")
+NODE_NAMES=("node1" "node2" "node3" "node4")
+
+usage () {
+    echo "usage: ./run-tor.sh [-p <.pem file> || -f <torrc file> || --with-tcp-dump || --with-inet-connection]"
+}
+
+HUGETLBFS_DIR="/mnt/huge"
+HUGETLBFS_NR=320
+
+W_TCP_DUMP=0
+W_INET_CONNECTION=0
+
+while [ "$1" != "" ]; do
+    
+    case $1 in
+
+		-f | --torrc-file )   			shift
+										TOR_TORRC=$1
+										;;
+		-p | --pem-file )   			shift
+										PEM_FILE=$1
+										;;
+        -t | --with-tcp-dump )			W_TCP_DUMP=1
+                                		;;
+        -i | --with-inet-connection )	W_INET_CONNECTION=1
+                                		;;
+		-h | --help	)					usage
+	        							exit
+	        							;;
+        * )								usage
+										exit 1
+    esac
+    shift
+
+done
+
+# 1) if specified, setup node-gw as an Internet gateway for the private Tor 
+# network
+
+# 2) run tor as deamon...
+INDEX=0
+
+for IP in ${NODE_IPS[@]}; do
+
+	# 2.1) if tcpdumps are to be collected, start them now...
+	if [[ $W_TCP_DUMP -eq 1 ]]; then
+
+		ssh -i $PEM_FILE $USER@$IP "'"tcpdump -i eth0 -s 0 -n -w $TOR_DATA_DIR/${NODE_NAMES[$INDEX]}.cap"'"
+	fi
+
+	ssh -i $PEM_FILE $USER@$IP "'"$TOR_DIR/$TOR_OR_DIR/$TOR_BINARY -f $TOR_DATA_DIR/$TOR_TORRC"'"
+
+	$((INDEX++))
+done
+
+# 3) if specified, periodically retrieve tcpdumps from targets...
+if [[ $W_TCP_DUMP -eq 1 ]]; then
+
+	cd $TOR_DATA_DIR
+
+	# infinite loop (don't judge me...)
+	KEEP_LOOPIN=1
+
+	while [[ $KEEP_LOOPIN -eq 1 ]]; do
+
+		INDEX=0
+
+		for IP in ${NODE_IPS[@]}; do
+
+			scp -i $PEM_FILE $USER@$IP:$TOR_DATA_DIR/${NODE_NAMES[$INDEX]}.cap .
+
+			$((INDEX++))
+		done
+
+		# sleep for 10 secs before re-collecting the tcpdumps...
+		sleep 10
+	done
+fi
+
+exit 0
